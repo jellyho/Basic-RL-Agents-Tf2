a6ebe7a10fa5e527035b01879f361dc0f943b232
jellyho <jellyho@naver.com>
2022-05-06 23:20:03 +0900

    Create SARSA.py

---
+++ SARSA.py
@@ -0,0 +1,31 @@
+import numpy as np
+import random
+from collections import defaultdict
+
+class SARSA:
+  def __init__(self, actions):
+    self.ACTIONS = np.array(actions)
+    self.step_size = 0.01
+    self.discount_factor = 0.9
+    self.epsilon = 0.1
+    self.q_table = defaultdict(lambda:[0.0 for _ in range(len(actions))])
+
+  def get_action(self, state):
+    if np.random.rand() < self.epsilon:
+        action = np.random.choice(self.ACTIONS)
+    else:
+        q_list = self.q_table[str(state)]
+        action = self.ACTIONS[np.random.choice(np.argwhere(q_list == np.amax(q_list)).flatten().tolist())]
+    return action
+
+  def learn(self, state, action, reward, next_state, next_action):
+    state, next_state = str(state), str(next_state)
+    current_q = self.q_table[state][np.argwhere(self.ACTIONS==action)[0][0]]
+
+    next_state_q = self.q_table[next_state][np.argwhere(self.ACTIONS==next_action)[0][0]]
+
+    td = reward + self.discount_factor * next_state_q - current_q
+
+    new_q = current_q + self.step_size * td
+
+    self.q_table[state][np.argwhere(self.ACTIONS==action)[0][0]] = new_q
